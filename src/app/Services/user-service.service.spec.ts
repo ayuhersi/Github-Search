aa718299c89257a76c7706b574044a54fcca44a4
Ayuhersi <ayub7hersi@gmail.com>
2022-02-22 00:21:10 +0300

    Adding services

--- src/app/Services/user-service.service.spec.ts
+++ src/app/Services/user-service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { User.ServiceService } from './user.service.service';
+import { UserServiceService } from '../user-service.service';
 
-describe('User.ServiceService', () => {
-  let service: User.ServiceService;
+describe('UserServiceService', () => {
+  let service: UserServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(User.ServiceService);
+    service = TestBed.inject(UserServiceService);
   });
 
   it('should be created', () => {
